c9e00f9386621727d422b24a2dd697281334784e
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-11 13:27:56 +0530

    Add files via upload

---
+++ classprogram.java
@@ -0,0 +1,9 @@
+class a{
+    int a1=5;
+}
+public class classprogram{
+    public static void main(String args[]){
+        a r=new a();//new is used for creation of instance variable
+        System.out.println(r.a1);
+    }
+}
