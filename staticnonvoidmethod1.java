603b903d589c59ef7f54313f19465e463ed3adf6
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-11 14:40:27 +0530

    Add files via upload

---
+++ staticnonvoidmethod1.java
@@ -0,0 +1,9 @@
+class staticnonvoidmethod1{
+    static String s(){
+        return "Hello jyo";
+    }
+    public static void main(String args[]){
+        String res=s();
+        System.out.println(res);
+    }
+}
