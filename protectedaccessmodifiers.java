f6a862878b5d7b54bfdd1f0e3badf843cfeb68fb
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-11 14:36:39 +0530

    Add files via upload

---
+++ protectedaccessmodifiers.java
@@ -0,0 +1,8 @@
+class protectedaccessmodifiers{
+    protected static void s(){
+        System.out.println("Hello Rithika");
+    }
+    public static void main(String args[]){
+        s();
+    }
+}
